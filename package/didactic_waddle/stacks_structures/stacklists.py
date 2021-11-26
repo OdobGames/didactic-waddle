7d468d989f65032d5deee3918388615aa75de5f6
YamiYume <p.steven.t.g@gmail.com>
2021-11-26 16:06:26 -0500

    stacklist

---
+++ package/didactic_waddle/stacks_structures/stacklists.py
@@ -0,0 +1,26 @@
+from typing import Any, NoReturn
+
+
+class stacklist:
+    def __init__(self, size: int) -> NoReturn:
+        self._size = size
+        self._stack = list()
+
+    def isEmpty(self) -> bool:
+        return not bool(self._stack)
+
+    def peek(self) -> Any:
+        try:
+            return self._stack[-1]
+        except KeyError:
+            pass
+
+    def push(self, value: Any) -> NoReturn:
+        if self._size < len(self.stack):
+            self._stack.append(value)
+        else:
+            pass
+
+    def pop(self) -> Any:
+        return self._stack.pop()
+    
