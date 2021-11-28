7e7f20ce97d5810a52ded2e0661515bd5d13cf7e
Odob13 <oordonezb@unal.edu.co>
2021-11-28 13:46:39 -0500

    Quevequeve

--- package/didactic_waddle/queves_structures/quevequeve.py
+++ package/didactic_waddle/queves_structures/quevequeve.py
@@ -1,24 +1,53 @@
 from queue import Queue
 from typing import Any, NoReturn
+from queveexceptions import EmptyException, FullException
 
 
 class QueveQueve:
-    def __init__(self, size: int = 0) -> NoReturn:
+    def __init__(self, size: int) -> NoReturn:
+
+        self._size = size
         self._queve = Queue(size)
 
     def isEmpty(self) -> bool:
+
         return self._queve.empty()
 
     def getFrontElement(self) -> Any:
-        pass
+
+        try:
+            temporal = self._queve.get()
+            temporal2 = temporal
+            for i in range(0, self._size):
+                if i == self._size-1:
+                    self._queve.put(temporal2)
+                    return temporal
+                else:
+                    self._queve.put(temporal2)
+                    temporal2 = self._queve.get()
+            return temporal
+        except IndexError:
+            raise EmptyException
 
     def getRearElement(self) -> Any:
-        pass
+
+        try:
+            for i in range(0, self._size):
+                temporal = self._queve.get()
+                self._queve.put(temporal)
+            return temporal
+        except IndexError:
+            raise EmptyException
 
     def put(self, n) -> NoReturn:
 
-        self._queve.put(n)
+        if self._size > self._queve.qsize():
+            self._queve.put(n)
+        else:
+            raise FullException
 
     def remove(self) -> NoReturn:
-
-        self._queve.get()
+        try:
+            self._queve.get()
+        except IndexError:
+            raise EmptyException
