017a2b726a577c14ee616431e374797e913921c6
Odob13 <oordonezb@unal.edu.co>
2021-12-01 23:12:46 -0500

    Cometarios quevelist

--- package/didactic_waddle/queves_structures/quevelist.py
+++ package/didactic_waddle/queves_structures/quevelist.py
@@ -1,11 +1,26 @@
 from typing import Any, NoReturn
 from queveexceptions import EmptyException, FullException
 
+""""Colas implementadas con listas de python"""
+
 
 class QueveList():
+
+    '''
+    Constructor:
+    Size= Tamaño de la cola.
+    Queve= Se crea una lista vacia.
+    '''
+
     def __init__(self, size: int) -> NoReturn:
         self._size = size
         self._queve = list()
+
+    '''
+    Función isEmpty:
+    Compara si la cola es igual una lista vacia,
+    si es asi retorna True, Si no retorna False.
+    '''
 
     def isEmpty(self) -> bool:
 
@@ -14,12 +29,28 @@ class QueveList():
         else:
             return False
 
+    '''
+    Función getFrontElement:
+    Busca el frente de la cola,
+    que es el elemento en la posicion 0 de la lista
+    Lo retorna si no hay ningun index error,
+    si no retorna una excepción personalizada.
+    '''
+
     def getFrontElement(self) -> Any:
 
         try:
             return self._queve[0]
         except IndexError:
             raise EmptyException
+
+    '''
+    Función getRearElement:
+    Busca el final de la cola, que es el ultimo elemento de la lista,
+    es decir la posicion tamaño de la lista menos 1.
+    Lo retorna si no hay ningun index error,
+    si no retorna una excepción personalizada.
+    '''
 
     def getRearElement(self) -> Any:
 
@@ -28,12 +59,25 @@ class QueveList():
         except IndexError:
             raise EmptyException
 
+    '''
+    Función put:
+    Recibe un valor de cualquier tipo y lo agrega al final de la cola.
+    Primero comprueba si el tamaño es el correcto, si es asi lo agrega,
+    de lo contrario retorna una excepcion personalizada.
+    '''
+
     def put(self, value: Any) -> NoReturn:
 
         if self._size > len(self._queve):
             self._queve.append(value)
         else:
             raise FullException
+
+    '''
+    Función remove:
+    Remueve el primer elemento al frente de la cola.
+    Si no hay elementos retorna una excepcion personalizada.
+    '''
 
     def remove(self) -> NoReturn:
 
