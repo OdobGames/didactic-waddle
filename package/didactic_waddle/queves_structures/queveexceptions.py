279e12861cfe9465cd5515a10a5a2923190ec66b
Odob13 <oordonezb@unal.edu.co>
2021-12-01 23:48:18 -0500

    Queveexceptions

--- package/didactic_waddle/queves_structures/queveexceptions.py
+++ package/didactic_waddle/queves_structures/queveexceptions.py
@@ -1,9 +1,22 @@
+
+'''
+EmptyException: Una excepcion personalizada para cuando la cola esta vacia,
+y genera un error.
+'''
+
+
 class EmptyException(Exception):
     '''Raised when the queve is empty and that generate a error'''
 
     def __init__(self, message="The queve is empty") -> None:
         self.message = message
         super().__init__(self.message)
+
+
+'''
+FullException: Una excepcion personalizada para cuando la cola esta llena,
+y no puede recibir nuevos valores.
+'''
 
 
 class FullException(Exception):
