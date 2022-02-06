2095454505f67070a9c182480de342f9b39a4c2a
Odob13 <oordonezb@unal.edu.co>
2022-02-06 13:51:50 -0500

    BST tree

--- package/tree_structures/BST_tree.py
+++ package/tree_structures/BST_tree.py
@@ -9,95 +9,93 @@ class Node:
         self.right = None
 
 
-# A utility function to do inorder traversal of BST
-def inorder(root):
-    if root is not None:
-        inorder(root.left)
-        print(root.key, end=" ")
-        inorder(root.right)
+class BST_tree:
+    # A utility function to do inorder traversal of BST
+    def inorder(self, root):
+        if root is not None:
+            self.inorder(root.left)
+            print(root.key, end=" ")
+            self.inorder(root.right)
 
+    # A utility function to insert a
+    # new node with given key in BST
+    def insert(self, node, key):
 
-# A utility function to insert a
-# new node with given key in BST
-def insert(node, key):
+        # If the tree is empty, return a new node
+        if node is None:
+            return Node(key)
 
-    # If the tree is empty, return a new node
-    if node is None:
-        return Node(key)
+        # Otherwise recur down the tree
+        if key < node.key:
+            node.left = self.insert(node.left, key)
 
-    # Otherwise recur down the tree
-    if key < node.key:
-        node.left = insert(node.left, key)
+        else:
+            node.right = self.insert(node.right, key)
 
-    else:
-        node.right = insert(node.right, key)
+        # return the (unchanged) node pointer
+        return node
 
-    # return the (unchanged) node pointer
-    return node
+    # Given a non-empty binary
+    # search tree, return the node
+    # with minimum key value
+    # found in that tree. Note that the
+    # entire tree does not need to be searched
 
-# Given a non-empty binary
-# search tree, return the node
-# with minimum key value
-# found in that tree. Note that the
-# entire tree does not need to be searched
+    def minValueNode(node):
+        current = node
 
+        # loop down to find the leftmost leaf
+        while(current.left is not None):
+            current = current.left
 
-def minValueNode(node):
-    current = node
+        return current
 
-    # loop down to find the leftmost leaf
-    while(current.left is not None):
-        current = current.left
+    # Given a binary search tree and a key, this function
+    # delete the key and returns the new root
 
-    return current
+    def deleteNode(self, root, key):
 
-# Given a binary search tree and a key, this function
-# delete the key and returns the new root
+        # Base Case
+        if root is None:
+            return root
 
+        # If the key to be deleted
+        # is smaller than the root's
+        # key then it lies in  left subtree
+        if key < root.key:
+            root.left = self.deleteNode(root.left, key)
 
-def deleteNode(root, key):
+        # If the kye to be delete
+        # is greater than the root's key
+        # then it lies in right subtree
+        elif(key > root.key):
+            root.right = self.deleteNode(root.right, key)
 
-    # Base Case
-    if root is None:
+        # If key is same as root's key, then this is the node
+        # to be deleted
+        else:
+
+            # Node with only one child or no child
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            # Node with two children:
+            # Get the inorder successor
+            # (smallest in the right subtree)
+            temp = self.minValueNode(root.right)
+
+            # Copy the inorder successor's
+            # content to this node
+            root.key = temp.key
+
+            # Delete the inorder successor
+            root.right = self.deleteNode(root.right, temp.key)
+
         return root
-
-    # If the key to be deleted
-    # is smaller than the root's
-    # key then it lies in  left subtree
-    if key < root.key:
-        root.left = deleteNode(root.left, key)
-
-    # If the kye to be delete
-    # is greater than the root's key
-    # then it lies in right subtree
-    elif(key > root.key):
-        root.right = deleteNode(root.right, key)
-
-    # If key is same as root's key, then this is the node
-    # to be deleted
-    else:
-
-        # Node with only one child or no child
-        if root.left is None:
-            temp = root.right
-            root = None
-            return temp
-
-        elif root.right is None:
-            temp = root.left
-            root = None
-            return temp
-
-        # Node with two children:
-        # Get the inorder successor
-        # (smallest in the right subtree)
-        temp = minValueNode(root.right)
-
-        # Copy the inorder successor's
-        # content to this node
-        root.key = temp.key
-
-        # Delete the inorder successor
-        root.right = deleteNode(root.right, temp.key)
-
-    return root
